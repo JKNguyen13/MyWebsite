59dffc8703c55f2d245daf7f2a6bd2b45cedd009
Justin Nguyen <jnguyen2737@student.pps.net>
2022-10-24 15:25:58 -0700

    Starting Calculator (10/24/22)

--- website-project/src/App.js
+++ website-project/src/App.js
@@ -1,14 +1,13 @@
 import logo from './logo.svg';
-import './App.css';
+//import './App.css';
+import './styles.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -17,16 +16,39 @@ function App() {
         >
           Learn React
         </a>
-        <ul>
-          <li>Hi</li>
-          <li>My</li>
-          <li>Name</li>
-          <li>Justin</li>
-          <li></li>
-        </ul>
+        
+
+        
         <div id="box"></div>
         <button id="button-Yes"> Yes </button>
         <button id="button-No"> No </button>
+        
+
+        <div className="calculator-grid">
+          <div className="output">
+            <div className="previous-operand">jackson+math</div>
+            <div className="current-operand">dumb</div>
+          </div>
+          <button className="span-two>">AC</button>
+          <button>DEL</button>
+          <button>/</button>
+          <button>1</button>
+          <button>2</button>
+          <button>3</button>
+          <button>*</button>
+          <button>4</button>
+          <button>5</button>
+          <button>6</button>
+          <button>+</button>
+          <button>7</button>
+          <button>8</button>
+          <button>9</button>
+          <button>-</button>
+          <button>.</button>
+          <button>0</button>
+          <button className="span-two">=</button>
+
+        </div>
 
       </header>
     </div>
