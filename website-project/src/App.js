e73a1e53cafaf3fb079610ee72812e72d9bae45a
Justin Nguyen <jnguyen2737@student.pps.net>
2022-11-16 14:46:27 -0800

    Added Calendar Layout & Tested With Checkboxes (11/16/22)

--- website-project/src/App.js
+++ website-project/src/App.js
@@ -20,8 +20,9 @@ function reducer(state, {type, payload}){
   }
 }
 
-const yes = ["'Don't look back.'", "'You are not a mistake.'", "'You are amazing.'", "'All our dreams can come true, if we have the courage to pursue them.'"]
+const yes = ["'Don't look back.'", "'You are not a mistake.'", "'You are amazing.'", "'Follow your dreams'"]
 const randNum = Math.floor(Math.random() * 4)
+const texts = ["hi"]
 
 function App() {
 
@@ -71,6 +72,45 @@ function App() {
 
         </div>
         
+        <div className = "calendar">
+          <div className ="dates">Sun Mon Tue Wed Thu Fri Sat</div>
+          <button id="first">27</button>
+          <button id="second">28</button>
+          <button id="third">29</button>
+          <button id="fourth">30</button>
+          <button>1</button>
+          <button>2</button>
+          <button>3</button>
+          <button>4</button>
+          <button>5</button>
+          <button>6</button>
+          <button>7</button>
+          <button>8</button>
+          <button>9</button>
+          <button>10</button>
+          <button>11</button>
+          <button>12</button>
+          <button>13</button>
+          <button>14</button>
+          <button>15</button>
+          <button>16</button>
+          <button>17</button>
+          <button>18</button>
+          <button>19</button>
+          <button>20</button>
+          <button>21</button>
+          <button>22</button>
+          <button>23</button>
+          <button>24</button>
+          <button>25</button>
+          <button>26</button>
+          <button>27</button>
+          <button>28</button>
+          <button>29</button>
+          <button>30</button>
+          <button>31</button>
+        </div>
+
         <div className = "quotesBox">
         <div className = "quotes">{yes[randNum]}</div>
         </div>
@@ -79,7 +119,13 @@ function App() {
         <div className="goals">GOALS:</div>
         <div className="custom" contenteditable="true">CUSTOM:</div>
           
-        <textarea className="reminders-box"></textarea> 
+        <div className="reminders-box">
+          <input type="checkbox" className="testC"></input>
+          <label for="testC">
+            <textarea>hi</textarea>
+          </label>
+        </div> 
+        
         <textarea className="goals-box"></textarea>
         <textarea className="custom-box"></textarea>
           
