c34e43cfbf86840b5732b590d6420f77cffa0402
Justin Nguyen <jnguyen2737@student.pps.net>
2022-12-16 15:27:14 -0800

    Improved DailyRec (12/16/22)

--- website-project/src/components/DailyRec.js
+++ website-project/src/components/DailyRec.js
@@ -1,11 +1,25 @@
 //import React, { createElement } from "react";
-import React, {useState} from 'react';
+import React, {useState, useCallback} from 'react';
 import './DailyRecStyle.css';
 
+const Item = ({id, removeDiv}) => {
+    const clickHandler = useCallback(() => {
+      removeDiv(id);
+    }, [id, removeDiv]);
+    //swap = 1;
+    return(
+      <div>
+        <input type="text" className="textBox"/>
+        <button onClick={clickHandler}>Submit</button>
+      </div>
+    );
+  };
 
-
-let hi = false;
-Boolean(hi);
+function Swapper(numSwap){
+    return(
+    numSwap = 1
+    );
+}
 
  function Yes(){
     /*return(
@@ -48,11 +62,22 @@ function secondRound(){
 
 function DailyRec(){
     const dailyRecLink = ["https://www.youtube.com/watch?v=sBspSJWRT2E&ab_channel=QueenOfficial","https://www.youtube.com/watch?v=kTJczUoc26U&ab_channel=TheKidLAROIVEVO","https://www.youtube.com/watch?v=Tc0tLGWIqxA&ab_channel=PostMaloneVEVO","https://www.youtube.com/watch?v=Brm1ribq7WU&ab_channel=NIKI-Topic"]
+    const songName = [" Bohemian Rhapsody", " Stay", " DNA", " Backburner"]
+    const artist = [" Queen", "The Kid LAROI, Justin Bieber", "Post Malone", "NIKI"]
     const randomGenre = [" Rock", " Pop", " Hip hop", " R&B"]
     const randNum = Math.floor(Math.random() * 4)
     const [count, setCount] = useState(0);
-    
 
+    const [items, setItems] = useState([]);
+    const addDiv = useCallback(() => {
+    setItems([...items, new Date().getTime()]);
+    }, [items]);
+    const removeDiv = useCallback((itemId) => {
+    setItems(items.filter((id) => id !== itemId));
+    }, [items]);
+    const fillerText = ["Did you enjoy this recommendation?", "Give us some feedback!", "Sorry, we'll try better next time!"]
+    var swap = 0;
+    
 return(
 
     <><div>
@@ -61,22 +86,31 @@ return(
     </div>
     
     <><div className="dailyRec">
-        <div className="text">Today's Recommendation: 
+        <div className="text"> Today's Song: 
             <a
-                className=""
+                className="songLink"
                 href={dailyRecLink[randNum]}
                 target="_blank"
                 rel="noopener noreferrer"
             >
-                {randomGenre[randNum]}
+                {songName[randNum]}
             </a>
+            <div>Artist: {artist[randNum]}</div>
+            <div>Genre: {randomGenre[randNum]}</div>
         </div>
     </div>
             <div className="dailyParent">
                 <div className="dailyRecFollowUp" id="dailyRecFollow">
-                    <div className="text">Did you enjoy this recommendation?
-                        <button onClick={secondRound}>Yes </button>
-                        <button onClick={No}>No</button>
+                    <div className="text">{fillerText[swap]}
+                    <div className="hope">
+                        {items.map((id) => (
+                        <Item key={id} id={id} removeDiv={removeDiv}/>
+                        ))}
+                        <button onClick={addDiv}>Yes</button>
+                        <button onClick={Swapper(swap)}>No</button>
+            </div>
+                        {/*<button onClick={secondRound}>Yes </button>
+                        <button onClick={No}>No</button>*/}
                     </div>
                 </div>
             </div>
